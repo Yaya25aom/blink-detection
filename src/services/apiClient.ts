8a1a686b4a9914eed0a7e7299abbc662154be266
yanisa <sirinyaausah46@gmail.com>
2026-09-19 21:03:19 +0700

    update cloudfare api url

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://choices-wav-tags-improve.trycloudflare.com/api";
+const API_URL = "https://alternatively-brass-tariff-low.trycloudflare.com/api";
 
 export const apiFetch = async (
   endpoint: string,
