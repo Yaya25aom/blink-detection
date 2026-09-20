dd4e074c097a0b91e905367ea28845408f9e7e2c
yanisa <sirinyaausah46@gmail.com>
2026-09-20 22:09:15 +0700

    update clodfare url login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,7 +18,7 @@ function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3000/api/auth/login", {
+      const response = await fetch("https://blank-wav-handbags-received.trycloudflare.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
